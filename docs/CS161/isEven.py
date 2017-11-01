c5f0144953f1ed9afb028f5bed0a008f39bc2268
David Lu <dlu@pdx.edu>
2017-11-01 17:59:32 +0800

    fixed isEven

--- docs/CS161/isEven.py
+++ docs/CS161/isEven.py
@@ -1,13 +1,13 @@
 def isEven(value):
     numbers = list(range(0, 100))
-    #print(numbers)
+    print(numbers)
     odd = False
-    #for num in range(0, 100):
-    for num in numbers:
+    for num in range(0, 100):
+    #for num in numbers:
         if odd:
             numbers.remove(num)
         odd = not odd
-        print(odd, num, numbers)
+        #print(odd, num, numbers)
     print(numbers)
     if value in numbers:
         return True
